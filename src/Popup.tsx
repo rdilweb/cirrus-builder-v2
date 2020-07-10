1731b1a3a4a1804d234c166c56ebddf63fb30ee0
Reece Dunham <me@rdil.rocks>
2020-07-10 19:19:33 +0000

    Make the popup TypeScript
    Signed-off-by: Reece Dunham <me@rdil.rocks>

---
+++ src/Popup.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
+
+interface Props {
+    handleClose(isOpen: boolean): void,
+    title: string,
+    desc: JSX.Element
+}
+
+export default (props: Props) => (
+    <Dialog
+        open={true}
+        onClose={() => props.handleClose(false)}
+        keepMounted
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+    >
+        <DialogTitle id="alert-dialog-slide-title">
+            {props.title}
+        </DialogTitle>
+        <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+                {props.desc}
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <Button
+                onClick={() => props.handleClose(false)}
+                color="primary"
+            >
+                Okay
+            </Button>
+        </DialogActions>
+    </Dialog>
+)
